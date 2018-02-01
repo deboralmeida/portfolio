82fd384dfe5726a3941e431539c726a33e707af7
débs <d.eboralmeida@hotmail.com>
2018-02-01 22:04:40 +0000

    backup

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
-// HORIZONTAL SCROLL 
+// HORIZONTAL SCROLL + INDICATIVE SCROLLBAR
+//var bar = document.getElementById('bar').clientWidth;
+//var grow = document.getElementById('grow').clientWidth;
+//var grow_W = document.getElementById('grow').clientWidth;
+//console.log(bar);
+//console.log(grow);
+//console.log(grow_W);
+var max = document.getElementById('gallery').clientWidth;
+var grow = document.getElementById('PROJECT_ID').clientWidth;
 var scroll = 0;
-var bar = 10;
 
 document.getElementById('wrapper').addEventListener('wheel', function(e) {
         
     //scroll up = scroll right
     if (e.deltaY < 0) {
-        scroll = scroll + 400;
+        
+        scroll = scroll + grow;
         this.scroll({
             left: scroll, 
-            behavior: 'smooth' 
+            behavior: 'smooth'
         });
+        
+        document.getElementById('grow').scroll({
+            left: scroll,
+            behavior: 'smooth'
+        })
         //console.log('scrolling up');
             
         //scrollbar grow + test
         //don't let bar go over 100%
-        if (bar == 100){
-            bar = 100;
-            document.getElementById('grow').style.width = bar+"%";
-            console.log(bar);
-        } 
-        else {
-            bar = bar + 10;
-            document.getElementById('grow').style.width = bar+"%";
-            console.log(bar);
-        }
+        /*if (grow >= bar){
+            
+            grow = bar;
+            document.getElementById('grow').style.width = grow+"px";
+            console.log(grow);
+            console.log(grow_W);
+            
+        } else {
+            
+            grow = grow + grow_W;
+            document.getElementById('grow').style.width = grow+"px";
+            console.log(grow);
+            console.log(grow_W);
+            
+        }*/
                 
-    }
+        if(scroll >= graphic_p - 50){
+            document.getElementById('graphiclink').style.color = "pink";
+        }
+        
+        if(scroll <= graphic_p - 50){
+            document.getElementById('graphiclink').style.color = "#ffffff";
+
+        }
+    };
+    
     //scroll down = scroll left
-    if (e.deltaY > 0) {  
-        scroll = scroll - 400;
+    if (e.deltaY > 0) { 
+        
+        scroll = scroll - grow;
         this.scroll({
             left: scroll, 
             behavior: 'smooth' 
@@ -37,145 +65,67 @@ document.getElementById('wrapper').addEventListener('wheel', function(e) {
         //console.log('scrolling down');  
             
         //scrollbar grow - test
-        //don't let bar go under 10
-        if (bar == 10) {
-            bar = 10;
-            document.getElementById('grow').style.width = bar+"%";
-            console.log(bar);
-        } else if (bar <= 100){
-            bar = bar - 10;
-            document.getElementById('grow').style.width = bar+"%";
-            console.log(bar);
-        } 
+        //don't let bar disappear and go into negative numbers
+        /*if (grow <= grow_W) {
             
+            grow = grow_W;
+            document.getElementById('grow').style.width = grow+"px";
+            console.log(grow);
+            console.log(grow_W);
+            
+        } else if (grow <= bar){
+            
+            grow = grow - grow_W;
+            document.getElementById('grow').style.width = grow+"px";
+            console.log(grow);
+            console.log(grow_W);
+            
+        } */
+            
+        if(scroll >= graphic_p - 50){
+            document.getElementById('graphiclink').style.color = "pink";
+        }
+        
+        if(scroll <= graphic_p - 50){
+            document.getElementById('graphiclink').style.color = "#ffffff";
+
+        }
+        
     }
         
     //scroll can't go under 0
-    if (scroll < 0){
+    if (scroll <= 0){
         scroll = 0;
+    }
+    
+    if (scroll >= max){
+        scroll = max;
     }
         
  });
 
 
-// ILLUSTRATION BUTTON
-document.getElementById('illustration_btn').onclick = function () {
-    
-    document.getElementById('graphicDesign_btn').classList.remove('selected');
-    document.getElementById('web_btn').classList.remove('selected');
-    document.getElementById('info_btn').classList.remove('selected');
-    
-    document.getElementById('graphicDesign_btn').classList.add('notSelected');
-    document.getElementById('web_btn').classList.add('notSelected');
-    document.getElementById('info_btn').classList.add('notSelected');
+// NAV SCROLLS SECTION INTO VIEW
+var web_p = document.getElementById('PROJECT_ID').offsetLeft;
+var graphic_p = document.getElementById('PROJECT_ID5').offsetLeft;
 
-    
+//update section's position when the window is resized
+window.addEventListener("resize", function(){
+    web_p = document.getElementById('PROJECT_ID').offsetLeft;
+    graphic_p = document.getElementById('PROJECT_ID5').offsetLeft;
+});
 
-    if (document.getElementById('graphicDesign_gallery').style.display == 'block') {   
-        document.getElementById('graphicDesign_gallery').style.display = 'none';      
-    } 
-    else if (document.getElementById('web_gallery').style.display == 'block') {       
-        document.getElementById('web_gallery').style.display = 'none';     
-    } 
-    else if (document.getElementById('moreInfo').style.display == 'block') {
-        document.getElementById('moreInfo').style.display = 'none';
-    }
+document.getElementById('graphiclink').addEventListener("click", function(){
+    //document.getElementById('wrapper').scrollLeft = graphic_p - 50;
+    document.getElementById('wrapper').scroll({
+            left: graphic_p - 50, 
+            behavior: 'smooth' 
+        });
+});
 
-    document.getElementById('illustration_gallery').style.display = 'block';
-    document.body.style.backgroundColor = '#d3d3d3';
-    document.getElementsByTagName('html')[0].style.backgroundColor = '#d3d3d3';
-
-    this.classList.remove('notSelected');
-    this.classList.add('selected');
-
-}
-
-// GRAPHIC DESIGN BUTTON
-document.getElementById('graphicDesign_btn').onclick = function() {
-    
-    document.getElementById('illustration_btn').classList.remove('selected');
-    document.getElementById('web_btn').classList.remove('selected');
-    document.getElementById('info_btn').classList.remove('selected');
-    
-    document.getElementById('illustration_btn').classList.add('notSelected');
-    document.getElementById('web_btn').classList.add('notSelected');
-    document.getElementById('info_btn').classList.add('notSelected');
-    
-    if ( document.getElementById('illustration_gallery').style.display == 'block' ){
-         document.getElementById('illustration_gallery').style.display = 'none';       
-    } 
-    else if ( document.getElementById('web_gallery').style.display == 'block' ){       
-         document.getElementById('web_gallery').style.display = 'none';     
-    } 
-    else if ( document.getElementById('moreInfo').style.display == 'block' ){    
-         document.getElementById('moreInfo').style.display = 'none';      
-    } 
-    
-    document.getElementById('graphicDesign_gallery').style.display = 'block';
-    document.body.style.backgroundColor = '#c0c0c0';
-    document.getElementsByTagName('html')[0].style.backgroundColor = '#c0c0c0';
-    
-    this.classList.remove('notSelected');
-    this.classList.add('selected');
-    
-}
-
-// WEB BUTTON
-document.getElementById('web_btn').onclick = function() {
-    
-    document.getElementById('graphicDesign_btn').classList.remove('selected');
-    document.getElementById('illustration_btn').classList.remove('selected');
-    document.getElementById('info_btn').classList.remove('selected');
-    
-    document.getElementById('graphicDesign_btn').classList.add('notSelected');
-    document.getElementById('illustration_btn').classList.add('notSelected');
-    document.getElementById('info_btn').classList.add('notSelected');
-    
-    if (  document.getElementById('illustration_gallery').style.display == 'block' ){
-         document.getElementById('illustration_gallery').style.display = 'none';       
-    } 
-    else if (  document.getElementById('graphicDesign_gallery').style.display == 'block' ){
-         document.getElementById('graphicDesign_gallery').style.display = 'none';     
-    } 
-    else if (  document.getElementById('moreInfo').style.display == 'block' ){    
-         document.getElementById('moreInfo').style.display = 'none';      
-    } 
-    
-    document.getElementById('web_gallery').style.display = 'block';
-    document.body.style.backgroundColor = '#a9a9a9';
-    document.getElementsByTagName('html')[0].style.backgroundColor = '#a9a9a9';
-    
-    this.classList.remove('notSelected');
-    this.classList.add('selected');
-    
-}
-
-// MORE INFO BUTTON
-document.getElementById('info_btn').onclick = function() {
-    
-    document.getElementById('graphicDesign_btn').classList.remove('selected');
-    document.getElementById('web_btn').classList.remove('selected');
-    document.getElementById('illustration_btn').classList.remove('selected');
-    
-    document.getElementById('graphicDesign_btn').classList.add('notSelected');
-    document.getElementById('web_btn').classList.add('notSelected');
-    document.getElementById('illustration_btn').classList.add('notSelected');
-    
-    if (  document.getElementById('illustration_gallery').style.display == 'block' ){
-         document.getElementById('illustration_gallery').style.display = 'none';       
-    } 
-    else if (  document.getElementById('graphicDesign_gallery').style.display == 'block' ){
-         document.getElementById('graphicDesign_gallery').style.display = 'none';     
-    } 
-    else if (  document.getElementById('web_gallery').style.display == 'block' ){       
-         document.getElementById('web_gallery').style.display = 'none';         
-    } 
-    
-    document.getElementById('moreInfo').style.display = 'block';
-    document.body.style.backgroundColor = '#808080';
-    document.getElementsByTagName('html')[0].style.backgroundColor = '#808080 ';
-    
-    this.classList.remove('notSelected');
-    this.classList.add('selected');
-    
-}
+document.getElementById('weblink').addEventListener("click", function(){
+    document.getElementById('wrapper').scroll({
+            left: web_p, 
+            behavior: 'smooth' 
+        });
+});
