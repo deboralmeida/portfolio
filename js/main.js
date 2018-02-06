18f57a91329c853ac0bb38d91146494f462b97d7
débs <d.eboralmeida@hotmail.com>
2018-02-06 01:29:44 +0000

    project navigation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-// HORIZONTAL SCROLL
-var max = document.getElementById('gallery').clientWidth;
+// ENABLE HORIZONTAL SCROLL 
+// AND HIGHLIGHT CURRENT PROJECT
 var scroll = 0;
-var x = document.getElementById('projecto').clientWidth;
+var scrollW = document.getElementById('projecto').clientWidth;
+var maxScroll = document.getElementById('gallery').clientWidth;
+
+var count = 0;
+var ball = document.getElementsByClassName("ball");
+var maxCount = ball.length -1;
+
+ball[count].classList.add('highlight');
 
 document.getElementById('wrapper').addEventListener('wheel', function(e) {
+    
+    ball[count].classList.remove('highlight');
     
     //scroll up = scroll right
     if (e.deltaY < 0) {
         
-        scroll = scroll + x;
+        scroll = scroll + scrollW;
         
         this.scroll({
             left: scroll, 
             behavior: 'smooth'
         });
         
-        
-        if(scroll > 0){
-            document.getElementById('ball').classList.remove('highlight');
-            document.getElementById('ball2').classList.add('highlight');
+        //scroll can't get over max
+        if (count >= maxCount) {
+            
+            count = maxCount;
+            ball[count].classList.add('highlight');
+            
+        } else {
+            
+            count = count + 1;
+            ball[count].classList.add('highlight');
+            
         }
         
     };
@@ -26,27 +42,36 @@ document.getElementById('wrapper').addEventListener('wheel', function(e) {
     //scroll down = scroll left
     if (e.deltaY > 0) { 
         
-        scroll = scroll - x;
+        scroll = scroll - scrollW;
         
         this.scroll({
             left: scroll, 
             behavior: 'smooth'
         });
         
-        if(scroll <= 0){
-            document.getElementById('ball').classList.add('highlight');
-            document.getElementById('ball2').classList.remove('highlight');
+        //scroll can't get under 0
+        if (count <= 0) {
+            
+            count = 0;
+            ball[count].classList.add('highlight');
+            
+        } else {
+            
+            count = count - 1;
+            ball[count].classList.add('highlight');
+            
         }
-        
+
     }
-        
+
     //scroll can't go under 0
-    if (scroll <= 0){
+    if (scroll <= 0) {
         scroll = 0;
     }
+    
     //scroll can't get over max
-    if (scroll >= max){
-        scroll = max;
+    if (scroll >= maxScroll) {
+        scroll = maxScroll;
     }
-        
+       
  });
